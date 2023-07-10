08a58b968e5688754d1534cfba8ca88c0277c3b1
csvcubed <csvcubed@gsscogs.uk>
2023-07-10 16:26:08 +0000

    Updating documentation.

--- inspect-api/_static/documentation_options.js
+++ inspect-api/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.5',
+    VERSION: '0.1.0.dev483190574249874690302564199211672092564516363017',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
